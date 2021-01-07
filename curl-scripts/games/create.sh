6493c6a797ad270c89ad70f9923b2dca0cb7f3f8
hankd11234 <hankd11234@gmail.com>
2021-01-07 14:16:39 -0500

    Copied game curl scripts from api

---
+++ curl-scripts/games/create.sh
@@ -0,0 +1,12 @@
+curl "http://localhost:4741/games" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "game": {
+      "title": "'"${TITLE}"'",
+      "releaseDate": "'"${DATE}"'",
+      "genre": "'"${GENRE}"'"
+    }
+  }'
