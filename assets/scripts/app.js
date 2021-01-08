7dcc45d0a206c600ba7a244361bd0488ebeddb4f
hankd11234 <hankd11234@gmail.com>
2021-01-08 10:46:35 -0500

    Finished index and CRUD event listeners, start api

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,7 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events')
+const gameEvents = require('./games/events')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
@@ -13,4 +14,9 @@ $(() => {
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#logout').on('submit', authEvents.onLogout)
+  $('#game-create').on('submit', gameEvents.onGameCreate)
+  $('#games-index').on('submit', gameEvents.onShowGames)
+  $('#game-show').on('submit', gameEvents.onGetGame)
+  $('#gamee-destroy').on('submit', gameEvents.onDestroyGame)
+  $('#game-update').on('submit', gameEvents.onUpdateGame)
 })
---
+++ assets/scripts/games/api.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const store = require('./../store')
+const config = require('./../config')
+
+const create = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/games',
+    method: 'POST',
+    // data: data
+    data,
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+const index = function () {
+  return $.ajax({
+    url: config.apiUrl + '/games',
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+const show = function (gameData) {
+  return $.ajax({
+    url: config.apiUrl + '/games/' + gameData.movie.id,
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+const destroy = function (gameData) {
+  return $.ajax({
+    url: config.apiUrl + '/games/' + gameData.movie.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+const update = function (gameData) {
+  return $.ajax({
+    url: config.apiUrl + '/games/' + gameData.movie.id,
+    method: 'PATCH',
+    data: gameData,
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  create,
+  index,
+  show,
+  destroy,
+  update
+}
